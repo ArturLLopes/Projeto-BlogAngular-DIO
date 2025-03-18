a135cf74efa15600303eea521f9dd6e9464a5846
ArturLLopes <arturllopes@gmail.com>
2025-03-18 16:09:01 -0300

    Feat: estrutura do projeto concluída

---
+++ src/app/components/small-card/small-topic/small-topic.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-small-topic',
+  standalone: false,
+  templateUrl: './small-topic.component.html',
+  styleUrl: './small-topic.component.css'
+})
+export class SmallTopicComponent {
+
+}
